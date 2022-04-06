3d3dfb0a3d0191efd271fb562d71008c00fd2e86
matheus.bockor <matheus.bockor@LAB1411.catolicasc.org.br>
2022-04-06 20:09:39 -0300

    test

--- src/br/org/catolicasc/banco/test/BancoListTest.java
+++ src/br/org/catolicasc/banco/test/BancoListTest.java
@@ -13,7 +13,7 @@ public class BancoListTest {
 
 	public static void main(String[] args) {
 		EntityManager em = JpaUtil.getEntityManger();
-		
+		//"aaaaa"
 		Query query = em.createQuery(" select a from Banco a ",Banco.class);
 		
 		List<Banco> bancos = query.getResultList();
